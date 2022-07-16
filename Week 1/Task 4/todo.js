7db090998da900f9aa59a0c4a7ff6c0fd4c70dd0
Blake Chamberlain <bchamberlain@Blakes-MBP.modem>
2022-07-16 15:46:12 +1000

    first commit

---
+++ Week 1/Task 4/todo.js
@@ -0,0 +1,24 @@
+let vue = new Vue({
+    el: "#app",
+    data:{
+        todos:[
+            {title:'Milk', checked: false},
+            {title:'Eggs', checked: false},
+            {title:'Apples', checked: false},
+            {title:'Bananas', checked: false},
+            {title:'Bread', checked: false},
+            {title:'Carrots', checked: false},
+            {title:'Chicken Breast', checked: false},
+            {title:'Lamb Chops', checked: false},
+            {title:'Salmon', checked: false},
+            {title:'Rump Steak', checked: false},
+            {title:'Potatoes', checked: false},
+            {title:'Toothpaste', checked: false},
+            {title:'Shampoo', checked: false},
+            {title:'Deodorant', checked: false},
+            {title:'Toilet Paper', checked: false},
+            {title:'Tissues', checked: false},
+            {title:'Soap', checked: false},
+        ],
+    },
+});
